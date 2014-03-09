6b030b2860278734d93e743efd780ab7433692ed
schoolie <brian.p.schoolcraft@gmail.com>
2014-03-09 18:22:25 -0400

    more order form work, added flash to base template

--- app/forms.py
+++ app/forms.py
@@ -3,7 +3,7 @@ from flask.ext.wtf import Form
 #from flask.ext.wtf import Required, Length
 
 from wtforms import TextField, BooleanField, TextAreaField
-from wtforms.validators import Required, Length, Email
+from wtforms.validators import Required, Length, Email, EqualTo
 
 class LoginForm(Form):
     openid = TextField('openid', validators = [Required()])
@@ -12,7 +12,8 @@ class LoginForm(Form):
 from app.models import User
 
 class OrderForm(Form):
-    email = TextField('email', validators = [Required(), Email(message=u'Invalid email address.')])
+    email = TextField('email', validators = [Required(), Email(message=u'Invalid email address.'), EqualTo('email2', message='Email address must match')])
+    email2 = TextField('email2')
     comments = TextAreaField('comments')
 
         
