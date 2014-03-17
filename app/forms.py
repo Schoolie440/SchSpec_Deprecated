1be77cfea53fd7e0b411505f77362834baccd7a9
schoolie <brian.p.schoolcraft@gmail.com>
2014-03-16 22:01:25 -0400

    added form macro file, integrated into smartswitch order form. need to debug, doesn't seem to be posting to views

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
 from flask.ext.wtf import Form, RecaptchaField
 
-from wtforms import TextField, BooleanField, TextAreaField, BooleanField
+from wtforms import TextField, BooleanField, TextAreaField, BooleanField, RadioField
 from wtforms.validators import Required, Length, Email, EqualTo
 
 class LoginForm(Form):
@@ -10,11 +10,14 @@ class LoginForm(Form):
 from app.models import User
 
 class OrderForm(Form):
-    email = TextField('email', validators = [Required(), Email(message=u'Invalid email address.'), EqualTo('email2', message='Email address must match')])
-    email2 = TextField('email2')
+    email = TextField('Enter your email:', validators = [Required(), Email(message=u'Invalid email address.'), EqualTo('email2', message='Email address must match')])
+    email2 = TextField('Confirm your email:')
     beta = BooleanField('beta')
-    comments = TextAreaField('comments')
+    comments = TextAreaField('Anything else you need to tell us?')
     captcha = RecaptchaField()
+    order_type = RadioField('Label', choices=[('beta','I want to be a Beta Tester!'),
+                                              ('standard',"I'll wait until Beta Testing is finished. Put me on the Pre-Order List!")])
+
 
         
 class PostForm(Form):
