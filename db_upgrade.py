59e1e281b3c09139c5f1e9c1be4e6e6aaacd5b7d
schoolie <brian.p.schoolcraft@gmail.com>
2014-03-28 20:30:37 -0400

    debug

--- db_upgrade.py
+++ db_upgrade.py
@@ -4,7 +4,7 @@ from config import SQLALCHEMY_DATABASE_URI
 from config import SQLALCHEMY_MIGRATE_REPO
 
 import os
-print os.environ['DATABASE_URL']
+
 
 print SQLALCHEMY_DATABASE_URI
 print SQLALCHEMY_MIGRATE_REPO
