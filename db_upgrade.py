233ef742a1b92fb466c7fbbe6b7ec933c1c6711e
schoolie <brian.p.schoolcraft@gmail.com>
2014-03-28 20:02:43 -0400

    debug

--- db_upgrade.py
+++ db_upgrade.py
@@ -2,5 +2,9 @@
 from migrate.versioning import api
 from config import SQLALCHEMY_DATABASE_URI
 from config import SQLALCHEMY_MIGRATE_REPO
+
+print SQLALCHEMY_DATABASE_URI
+print SQLALCHEMY_DATABASE_REPO
+
 api.upgrade(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
 print 'Current database version: ' + str(api.db_version(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO))
