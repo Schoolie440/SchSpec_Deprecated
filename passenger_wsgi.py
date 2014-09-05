0eac0dc8d036ed7a76a9a411fc9b81594c7f1de4
schoolie <brian.p.schoolcraft@gmail.com>
2014-09-04 20:58:04 -0400

    updated passenger_wsgi to set database url

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -2,4 +2,22 @@ import sys, os
 virt_binary = "/home/schoolcr/testenv/bin/python"
 if sys.executable != virt_binary: os.execl(virt_binary, virt_binary, *sys.argv)
 sys.path.append(os.getcwd())
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+if basedir == '/home/schoolcr/SchSpec':
+  RUN_TYPE = 'production'
+elif basedir == '/home/schoolcr/SchSpecTest':
+  RUN_TYPE = 'test'
+else:
+  RUN_TYPE = 'local'
+
+if RUN_TYPE == 'production':
+  os.environ['SS_DATABASE_URL'] = 'mysql://schoolcr_SchSpec:specialties14@localhost/schoolcr_SchSpec'
+elif RUN_TYPE == 'test':
+  os.environ['SS_DATABASE_URL'] = 'mysql://schoolcr_SchSpec:specialties14@localhost/schoolcr_SchSpecTest'
+else: 
+  print 'Environment invalid'
+
+SS_DATABASE_URL
 from app import app as application
