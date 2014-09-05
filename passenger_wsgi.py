f0e35b4e0fe308bb65866ab98f6fed234ac6772c
schoolcr <schoolcr@lagertha.asmallorange.com>
2014-09-04 20:44:29 -0400

    added passenger_wsgi

---
+++ passenger_wsgi.py
@@ -0,0 +1,5 @@
+import sys, os
+virt_binary = "/home/schoolcr/testenv/bin/python"
+if sys.executable != virt_binary: os.execl(virt_binary, virt_binary, *sys.argv)
+sys.path.append(os.getcwd())
+from app import app as application
