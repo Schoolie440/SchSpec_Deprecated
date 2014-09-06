606330427011567eae96476f8650b3ce248d8a0a
schoolie <brian.p.schoolcraft@gmail.com>
2014-09-05 20:06:20 -0400

    added environment check to virtualenv location

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,7 +1,4 @@
 import sys, os
-virt_binary = "/home/schoolcr/testenv/bin/python"
-if sys.executable != virt_binary: os.execl(virt_binary, virt_binary, *sys.argv)
-sys.path.append(os.getcwd())
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -11,12 +8,23 @@ elif basedir == '/home/schoolcr/SchSpecTest':
   RUN_TYPE = 'test'
 else:
   RUN_TYPE = 'local'
+  
+
+
+
 
 if RUN_TYPE == 'production':
   os.environ['SS_DATABASE_URL'] = 'mysql://schoolcr_SchSpec:specialties14@localhost/schoolcr_SchSpec'
+  virt_binary = "/home/schoolcr/pyenv/bin/python"
+
 elif RUN_TYPE == 'test':
   os.environ['SS_DATABASE_URL'] = 'mysql://schoolcr_SchSpec:specialties14@localhost/schoolcr_SchSpecTest'
+  virt_binary = "/home/schoolcr/testenv/bin/python"
+
 else: 
   print 'Environment invalid'
+  
+if sys.executable != virt_binary: os.execl(virt_binary, virt_binary, *sys.argv)
+sys.path.append(os.getcwd())
 
 from app import app as application
